29c3514f5587d2ace42802f243d767fbc8ee2e4e
lrbn86 <lrbn86@gmail.com>
2022-10-04 17:10:56 -0400

    implemented API backend

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,10 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { projectAPI } from "./projectAPI";
 import { MOCK_PROJECTS } from "./MockProjects";
 import { Project } from "./Project";
 import ProjectList from "./ProjectList";
 
 function ProjectsPage() {
-    const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+    // const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
+    const [projects, setProjects] = useState<Project[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | undefined>(undefined);
+    useEffect(() => {
+        async function loadProjects() {
+            setLoading(true);
+            try {
+                const data = await projectAPI.get(1);
+                setError("");
+                setProjects(data);
+            } catch (e) {
+                if (e instanceof Error) {
+                    setError(e.message);
+                }
+            } finally {
+                setLoading(false);
+            }
+        }
+        loadProjects();
+    }, []);
     const saveProject = (project: Project) => {
         let updatedProjects = projects.map((p: Project) => {
             return p.id === project.id ? project : p;
@@ -14,7 +35,25 @@ function ProjectsPage() {
     return (
         <>
             <h1>Projects</h1>
+            {error && (
+                <div className="row">
+                    <div className="card large error">
+                        <section>
+                            <p>
+                                <span className="icon-alert inverse"></span>
+                                {error}
+                            </p>
+                        </section>
+                    </div>
+                </div>
+            )}
             <ProjectList onSave={saveProject} projects={projects} />
+            {loading && (
+                <div className="center-page">
+                    <span className="spinner primary"></span>
+                    <p>Loading...</p>
+                </div>
+            )}
         </>
     );
 }
--- src/projects/projectAPI.ts
+++ src/projects/projectAPI.ts
@@ -51,7 +51,6 @@ const convertToProjectModel = (item: any): Project => {
 const projectAPI = {
     get(page = 1, limit = 20) {
         return fetch(`${url}?_page${page}&_limit=${limit}&_sort=name`)
-            .then(delay(600))
             .then(checkStatus)
             .then(parseJSON)
             .then(convertToProjectModels)
@@ -63,3 +62,5 @@ const projectAPI = {
             });
     },
 };
+
+export { projectAPI };
