2658d86c1d991ea04c1d33e00259748753e8a51a
lrbn86 <lrbn86@gmail.com>
2022-10-04 17:16:20 -0400

    added pagination

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -9,13 +9,17 @@ function ProjectsPage() {
     const [projects, setProjects] = useState<Project[]>([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | undefined>(undefined);
+    const [currentPage, setCurrentPage] = useState(1);
     useEffect(() => {
         async function loadProjects() {
             setLoading(true);
             try {
-                const data = await projectAPI.get(1);
-                setError("");
-                setProjects(data);
+                const data = await projectAPI.get(currentPage);
+                if (currentPage === 1) {
+                    setProjects(data);
+                } else {
+                    setProjects((projects) => [...projects, ...data]);
+                }
             } catch (e) {
                 if (e instanceof Error) {
                     setError(e.message);
@@ -25,7 +29,10 @@ function ProjectsPage() {
             }
         }
         loadProjects();
-    }, []);
+    }, [currentPage]);
+    const handleMoreClick = () => {
+        setCurrentPage((currentPage) => currentPage + 1);
+    };
     const saveProject = (project: Project) => {
         let updatedProjects = projects.map((p: Project) => {
             return p.id === project.id ? project : p;
@@ -48,6 +55,20 @@ function ProjectsPage() {
                 </div>
             )}
             <ProjectList onSave={saveProject} projects={projects} />
+            {!loading && !error && (
+                <div className="row">
+                    <div className="col-sm-12">
+                        <div className="button-group fluid">
+                            <button
+                                className="button default"
+                                onClick={handleMoreClick}
+                            >
+                                More...
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            )}
             {loading && (
                 <div className="center-page">
                     <span className="spinner primary"></span>
