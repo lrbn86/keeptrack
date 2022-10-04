f18d1729ea021eb7b9eef0d1a33ac254715eeee4
lrbn86 <lrbn86@gmail.com>
2022-10-03 21:30:32 -0400

    fix: formatting

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import "./App.css";
 import ProjectsPage from "./projects/ProjectsPage";
 
 function App() {
-  return (
-    <div className="container">
-      <ProjectsPage />
-    </div>
-  );
+    return (
+        <div className="container">
+            <ProjectsPage />
+        </div>
+    );
 }
 
 export default App;
--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -4,20 +4,20 @@ import ProjectCard from "./ProjectCard";
 import ProjectForm from "./ProjectForm";
 
 interface ProjectListProps {
-  projects: Project[];
+    projects: Project[];
 }
 
 function ProjectList({ projects }: ProjectListProps) {
-  return (
-    <div className="row">
-      {projects.map((project) => (
-        <div key={project.id} className="cols-sm">
-          <ProjectCard project={project} />
-          <ProjectForm />
+    return (
+        <div className="row">
+            {projects.map((project) => (
+                <div key={project.id} className="cols-sm">
+                    <ProjectCard project={project} />
+                    <ProjectForm />
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 }
 
 export default ProjectList;
