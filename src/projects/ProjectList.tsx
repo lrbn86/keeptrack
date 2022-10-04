34c806d2e0f20dbe653e0b14d799db356eb7abd9
lrbn86 <lrbn86@gmail.com>
2022-10-04 05:55:20 -0400

    setup form events

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -5,19 +5,23 @@ import ProjectForm from "./ProjectForm";
 
 interface ProjectListProps {
     projects: Project[];
+    onSave: (project: Project) => void;
 }
 
-function ProjectList({ projects }: ProjectListProps) {
+function ProjectList({ projects, onSave }: ProjectListProps) {
     const [projectBeingEdited, setProjectBeingEdited] = useState({});
     const handleEdit = (project: Project) => {
         setProjectBeingEdited(project);
+    };
+    const cancelEditing = () => {
+        setProjectBeingEdited({});
     };
     return (
         <div className="row">
             {projects.map((project) => (
                 <div key={project.id} className="cols-sm">
                     {project === projectBeingEdited ? (
-                        <ProjectForm />
+                        <ProjectForm onSave={onSave} onCancel={cancelEditing} />
                     ) : (
                         <ProjectCard project={project} onEdit={handleEdit} />
                     )}
--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { MOCK_PROJECTS } from "./MockProjects";
+import { Project } from "./Project";
 import ProjectList from "./ProjectList";
 
 function ProjectsPage() {
+    const saveProject = (project: Project) => {
+        console.log("Saving project:", project);
+    };
     return (
         <>
             <h1>Projects</h1>
-            <ProjectList projects={MOCK_PROJECTS} />
+            <ProjectList onSave={saveProject} projects={MOCK_PROJECTS} />
         </>
     );
 }
